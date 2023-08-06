1edef20fe618cf4d8cd63ca4693fc10769c47946
shabeeboali@gmail.com <shabeeboali@gmail.com>
2023-08-06 08:25:03 +0530

    config

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,17 +1,32 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 
-	"github.com/bashbeebo/gommerce/handlers"
+	"github.com/bashbeebo/gommerce/pkg/config"
+	"github.com/bashbeebo/gommerce/pkg/handlers"
+	"github.com/bashbeebo/gommerce/pkg/render"
 )
 
 const port string = ":8080"
+
 func main() {
-	http.HandleFunc("/",handlers.Home)
-	err := http.ListenAndServe(port,nil)
+	var app config.AppConfig
+
+	tc, err := render.CreateTemplateCache()
 	if err != nil {
-		log.Println(err)
+		log.Fatal("cannot create template cache")
 	}
+
+	app.TemplateCache = tc
+
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+	render.NewTemplates(&app)
+	http.HandleFunc("/", handlers.Repo.Home)
+	fmt.Printf("Starting Application on port %s", port)
+	_ = http.ListenAndServe(port, nil)
+
 }
--- pkg/render/render.go
+++ pkg/render/render.go
@@ -2,41 +2,41 @@ package render
 
 import (
 	"bytes"
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
 	"path/filepath"
+
+	"github.com/bashbeebo/gommerce/pkg/config"
 )
 
+var app *config.AppConfig
+
+func NewTemplates(a *config.AppConfig) {
+	app = a
+}
 func RenderTemplate(w http.ResponseWriter, tmpl string) {
-	//create template cache
-	tc, err := createTemplateCache()
-	if err != nil {
-		log.Fatal(err)
-	}
-	// get requested template cache
+	tc := app.TemplateCache
 
 	t, ok := tc[tmpl]
 
 	if !ok {
-		log.Fatal(err)
+		log.Fatal("Could not get template from template cache")	
 	}
 
 	buf := new(bytes.Buffer)
 
-	err = t.Execute(buf, nil)
-	if err != nil {
-		log.Println(err)
-	}
+	_ = t.Execute(buf, nil)
 
 	// render the template
-	_, err = buf.WriteTo(w)
+	_, err := buf.WriteTo(w)
 	if err != nil {
-		log.Println(err)
+		fmt.Println("Error writing template to browser", err)
 	}
 }
 
-func createTemplateCache() (map[string]*template.Template, error) {
+func CreateTemplateCache() (map[string]*template.Template, error) {
 	myCache := map[string]*template.Template{}
 
 	//get all of the files name named *.page.tmpl from ./templates
